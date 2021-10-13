961d5ec99bc0ed56f6f8ed4f96788ac3293e4af4
vutrongminh27 <vutrongminh05@gmail.com>
2021-10-13 15:32:39 +0700

    a

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname+"/assert/home.html"))
 })
 
-app.listen(PORT, 'localhost', function(){
-    console.log(`Server is running on port ${PORT}.`);
+app.listen(5000, 'localhost', function(){
+    console.log(`Server is running on port 5000.`);
 });
     
