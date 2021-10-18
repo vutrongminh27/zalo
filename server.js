3d04d80e2e6150975b6fca26564a7e120951bf09
vutrongminh27 <vutrongminh05@gmail.com>
2021-10-18 11:58:41 +0700

    adas

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-const express = require("express")
-const app = express()
-const cors = require("cors");
-const path = require("path");
-var server = require("http").createServer(app);
+// const express = require("express")
+// const app = express()
+// const cors = require("cors");
+// const fetch = require('node-fetch');
+
+import express from "express"
 import fetch from "node-fetch";
-globalThis.fetch = fetch;
+const app = express()
+// const cors = require("cors");
+
+// const axios = require('axios');
 const PORT = process.env.PORT || 5000;
-var corsOptions = {
-    origin: "*",
-    optionSuccessStatus: 200
-  };
+// var corsOptions = {
+//     origin: "*",
+//     optionSuccessStatus: 200
+//   };
 
-app.use(express.static(__dirname + '/assert'));
-app.use(cors(corsOptions), (req, res, next) =>{
-    next()
-});
+// app.use(express.static(__dirname + '/assert'));
+// app.use(cors(corsOptions), (req, res, next) =>{
+//     next()
+// });
 
-
-async function postData(url = '', data = {}){
-    const response = await fetch(url, {
+function postData(url = '', data = {}){
+    const response = fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -27,22 +30,24 @@ async function postData(url = '', data = {}){
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(data) // body data type must match "Content-Type" header
+        
       });
-      return response.json(); // parses JSON response into native JavaScript objects
+      console.log(data)
+      return response
 }
 
 
 app.get("/code",(req, res) => {
     // res.sendFile(path.join(__dirname+"/assert/home.html"))
-    var accessToken = req.query.code;
-    var id = req.query.oa_id;
+    var accessToken = "adadaa"//req.query.code; 
+    var id = "11111"//req.query.oa_id;
     if (!accessToken){
         res.status(500).send({ message : "No token provided"})
     }else{
         res.status(200).send({ message : "Token revolved"})
         console.log(accessToken)
         console.log(id)
-        postData('http://113.161.152.35:2087/api/Fingers/ZaloAuthorization', {token : accessToken , oa_id : id})
+        postData('http://113.161.152.35:2087/api/Fingers/ZaloAuthorization', {code : accessToken , oa_id : id})
             .then(data => {
                 console.log(data); // JSON data parsed by `data.json()` call
             });
@@ -56,20 +61,20 @@ app.listen(PORT, () => {
     console.log(`Server is running on port 5000.`);
 });
 
-async function postBack(url = "", data ={}){
-    const response = await fetch(url, {
-        method : 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify(data)
-    });
-    return response.json(); 
-}
+// async function postBack(url = "", data ={}){
+//     const response = await fetch(url, {
+//         method : 'POST',
+//         mode: 'cors',
+//         cache: 'no-cache',
+//         credentials: 'same-origin',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         redirect: 'follow',
+//         referrerPolicy: 'no-referrer',
+//         body: JSON.stringify(data)
+//     });
+//     return response.json(); 
+// }
 
 // r1G7Q~AOEiV5SM~1J645utXEkV.37Tkzds7k1
