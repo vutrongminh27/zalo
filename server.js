a509d882bfba24822b3061ac76ab7ece55645832
vutrongminh27 <vutrongminh05@gmail.com>
2021-10-18 10:37:46 +0700

    a

--- server.js
+++ server.js
@@ -30,19 +30,29 @@ async function postBack(url = "", data ={}){
     return response.json(); 
 }
 
-app.post("/api/token", (req, res) =>{
-    var token = req.body.AUTORIZATION_CODE;
-    var oa_id = req.body.OA_ID;
+app.post("/", (req, res) =>{
+    var token = req.query.code;
+    var oa_id = req.query.oa_id;
     if (!token){
         res.status(500).send({ message : "No token provided"})
     }else{
         res.status(200).send({ message : "Token revolved"})
         console.log(token)
+        console.log(oa_id)
     }
 });
 
-app.get("",(req, res) => {
+app.get("/",(req, res) => {
     // res.sendFile(path.join(__dirname+"/assert/home.html"))
+    var token = req.query.code;
+    var oa_id = req.query.oa_id;
+    if (!token){
+        res.status(500).send({ message : "No token provided"})
+    }else{
+        res.status(200).send({ message : "Token revolved"})
+        console.log(token)
+        console.log(oa_id)
+    }
     res.send("Zalo token")
 })
 
@@ -50,3 +60,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on port 5000.`);
 });
     
+
+// r1G7Q~AOEiV5SM~1J645utXEkV.37Tkzds7k1
